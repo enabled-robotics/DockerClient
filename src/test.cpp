a52fac25771dd25ea9725a3a84a2c8ea57ab24fc
Niels Hvid <nielshvid@gmail.com>
2021-05-20 12:17:00 +0200

    Restructured DockerClient to build with cmakelists

---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include "docker.h"
+
+int main(){
+    Docker client = Docker();
+
+    JSON_DOCUMENT doc = client.docker_version(); 
+    std::cout << jsonToString(doc) << std::endl;
+
+    return 0;
+}
